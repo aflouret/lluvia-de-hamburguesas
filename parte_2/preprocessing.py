15cfa400549f0c5f3214c1a25db9f7b33ff82114
facundo <facundomilhas@gmail.com>
2021-11-25 01:37:33 -0300

    Arrancamos a hacer el preprocessing.py

--- parte_2/preprocessing.py
+++ parte_2/preprocessing.py
@@ -2,9 +2,26 @@ import numpy as np
 import pandas as pd
 
 
-def preprocess(df):
-    df['presion_atmosferica_tarde'] = pd.to_numeric(df['presion_atmosferica_tarde'],errors='coerce')
-    df = df.dropna(subset=['llovieron_hamburguesas_al_dia_siguiente'])
+def preprocess(df:pd.DataFrame):
+    
+    #Convertimos la presion a un tipo numérico, establecimos los valores inválidos como NaN.
+    df['presion_atmosferica_tarde'] = pd.to_numeric(df['presion_atmosferica_tarde'],errors='coerce')    
+    #Convertimos las fechas al tipo datetime.
     df['dia'] = pd.to_datetime(df['dia'])
+    
+    #Eliminamos todas las filas del dataframe donde haya valores faltantes
+    df = df.dropna(subset=['llovieron_hamburguesas_al_dia_siguiente']) 
+    
+    #Eliminamos las dos columnas con mayor porcentaje de missings
+    df = df.drop(columns = ['horas_de_sol', 'mm_de_agua_avaporados']) 
+    
+    #Llenamos las columnas con grandes porcentajes de missings con la mediana
+    df['nubosidad_tarde'] = df['nubosidad_tarde'].fillna(df['nubosidad_tarde'].median())
+    
+    
+        
+   
+    return df
+  
     
 
